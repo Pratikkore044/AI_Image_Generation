edefbd9becb1bde9e210ebb9fe63fc473cfede8b
Pratik <iampratikkore@gmail.com>
2025-03-01 14:58:13 +0530

    Added URL  Backend(renderURL)

--- client/src/api/index.jsx
+++ client/src/api/index.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const API = axios.create({
-    baseURL:"http://localhost:8080/api/",
+    baseURL:"https://ai-image-generation-vl4q.onrender.com/api/",
 });
 
 export const GetPosts = async() => await API.get("/post/");
